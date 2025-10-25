bda1743e2f715c10736b695a9ea5bbf495da51c2
iTsmE-NY <pny12335@gmail.com>
2025-10-25 14:01:39 +0900

    test: test 문구 추가

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="p-5 flex flex-col gap-10">
       <main className="w-[400px] h-[400px] outline !outline-black flex justify-center items-center">
-        <div className="bg-black size-20"></div>
+        <div className="bg-black size-20">TEST</div>
       </main>
       <main className="w-[400px] h-[400px] outline !outline-black relative">
         <div className="bg-black h-5"></div>
